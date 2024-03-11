8c4f16b7f88700bd0a820869f753eaece5a14838
tony.tian <tony.tian@fdmgroup.com>
2024-03-11 11:18:43 +0800

    first commit

---
+++ src/main/java/com/fdmproject/datamigration/DatamigrationApplication.java
@@ -0,0 +1,13 @@
+package com.fdmproject.datamigration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamigrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamigrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fdmproject/datamigration/DatamigrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdmproject.datamigration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamigrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
