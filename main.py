c7c42c8a9c2d33d483b1de34ad51a57e14d614be
Sam Artigolle <sam.artigolle@fdmgroup.com>
2024-03-22 11:59:35 +0800

    Add flask_cors to support OSX clients

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from api.credentials import credentials_blueprint
 from api.schema_data import schema_data_blueprint
 from api.migrate import migrate_blueprint
@@ -7,6 +8,7 @@ import secrets
 
 
 app = Flask(__name__)
+cors = CORS(app, supports_credentials=True)
 app.secret_key = secrets.token_hex(16)
 app.register_blueprint(credentials_blueprint)
 app.register_blueprint(schema_data_blueprint)
