192fb26b958758901f39f4c97fbab30d238cdd81
Sam Artigolle <sam.artigolle@fdmgroup.com>
2024-03-27 13:41:10 +0800

    fix CORS issue when running dockerized frontend

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import secrets
 app = Flask(__name__)
 cors = CORS(app, supports_credentials=True)
 app.secret_key = secrets.token_hex(16)
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
 app.register_blueprint(credentials_blueprint)
 app.register_blueprint(schema_data_blueprint)
 app.register_blueprint(migrate_blueprint)
